6f394e35e990c5367e9b9d7e89c5368987140d0f
utcizumi masato <u.masato0808@gmail.com>
2017-03-02 19:24:49 +0900

    edit redirect_to

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,7 +19,7 @@ class GroupsController < ApplicationController
   def update
     group = Group.find(params[:id])
     group.update(group_params)
-    redirect_to "/groups"
+    redirect_to groups_path
   end
 
   private
