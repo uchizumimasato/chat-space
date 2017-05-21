797b023149ad17136887a6044c215072d5e49312
utcizumi masato <u.masato0808@gmail.com>
2017-05-22 01:28:33 +0900

    add scope user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def search
-    @users = User.where('name LIKE(?)', "%#{search_params}%").order('name ASC')
+    @users = User.users params[:name]
     respond_to do |format|
       format.json { render json: @users }
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   has_many :groups, through: :group_users
   has_many :group_users
   has_many :messages
+
+  scope :users, -> (name){where('name LIKE(?)', "%#{name}%").order('name ASC')}
 end
