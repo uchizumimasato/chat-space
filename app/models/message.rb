90e3b232ded9470876ae8f0b56d44c72ab86e51c
utcizumi masato <u.masato0808@gmail.com>
2017-05-27 18:38:48 +0900

    edit single quotes

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
-  validates :body, presence: true, if: "image.blank?"
+  validates :body, presence: true, if: 'image.blank?'
   mount_uploader :image, ImageUploader
 end
