b8bdc3283ba2d2c5efc36cf8935164b3f0cf8e4e
utcizumi masato <u.masato0808@gmail.com>
2017-05-28 14:53:48 +0900

    edit message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -6,11 +6,11 @@ $(function() {
     var body       = `<div class="chat-body-message__text"> ${ message.body } </div>`;
     var image      = `<div class="chat-body-message__text"> <img src="${ message.image }" class="chat-image"></div>`;
     if (message.image == undefined) {
-      $('.chat-body-message').append(name, created_at, body);
+      $('.chat-body').append(name, created_at, body);
     } else if (message.body == undefined) {
-      $('.chat-body-message').append(name, created_at, image);
+      $('.chat-body').append(name, created_at, image);
     } else {
-      $('.chat-body-message').append(name, created_at, body, image);
+      $('.chat-body').append(name, created_at, body, image);
     }
   }
 
@@ -18,6 +18,7 @@ $(function() {
     e.preventDefault();
     var formdata  = new FormData($('#new_message').get(0));
     $('.chat-footer-button').removeAttr('data-disable-with');
+    console.log('aaaaaa');
     $.ajax({
       type:        'POST',
       data:        formdata,
