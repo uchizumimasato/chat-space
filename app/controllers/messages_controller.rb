9614e35a4996a25716e5c6decac7babdd742e893
utcizumi masato <u.masato0808@gmail.com>
2017-06-02 14:49:54 +0900

    edit params

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -38,7 +38,6 @@ class MessagesController < ApplicationController
   end
 
   def auto_params
-    new_messages = Message.where("id > #{params[:message_id]}", group_id: params[:group_id]).where.not(user_id: current_user.id).map{ |new_message| message_js(new_message)}
-    return new_messages
+    Message.where("id > #{params[:message_id]}", group_id: params[:group_id]).where.not(user_id: current_user.id).map{ |new_message| message_js(new_message)}
   end
 end
